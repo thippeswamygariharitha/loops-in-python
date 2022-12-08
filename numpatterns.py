4a8a659c43ddd7b7fcafc4c238a495aa34cac416
thippeswamygariharitha <112331384+thippeswamygariharitha@users.noreply.github.com>
2022-12-08 10:29:50 +0530

    Add files via upload

---
+++ numpatterns.py
@@ -0,0 +1,131 @@
+
+
+
+# a=int(input('enter rows:'))
+# for i in range(1,a):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print()
+# OutputChecker
+# 0
+# 0 1
+# 0 1 2
+# 0 1 2 3
+
+
+# a=int(input('enter rows:'))
+# for i in range(a,0,-1):
+#     for j in range(0,i):
+#         print(j,end=' ')
+#     print()
+
+# a=int(input('enter rows:'))
+# for i in range(a,1,-1):
+#     for j in range(1,i):
+#         print(j,end=' ')
+#     print()
+# OutputChecker
+# 0 1 2 3 4
+# 0 1 2 3
+# 0 1 2
+# # 0 1
+# # 0
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i,a):
+#         print(' ',end=' ')
+#     for j in range(i,-1,-1):
+#         print(j+1,end=' ')
+#     print()
+#       1
+#      21
+#     321
+#    4321
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for j in range(i,a):
+#         print(j,end=' ')
+#     print()    
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for j in range(a-i,0,-1):
+#         print(j,end=' ')
+#     print()
+# 5 4 3 2 1
+#   4 3 2 1
+#     3 2 1
+#       2 1
+#         1
+
+# a=1
+# while a<=3:
+#     print(a,end='')
+#     a+=1
+# else:
+#     print(a)
+
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i):
+#         print(i,end=' ')
+#     print()
+# OutputChecker
+
+# 1
+# 22
+# 333
+# 4444
+
+
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for j in range(a-i):
+#         print(a-i,end=' ')
+#     print()
+# OutputChecker
+# 55555
+#  4444 
+#   333
+#    22
+# #     1
+    
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i):
+#         print(j,end=' ')
+#     print() 
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i+1):
+#         print(j+1,end=' ')
+#     print()
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i,a):
+#         print(i,end=' ')
+#     print()
+
+# a=int(input('enter rows:'))
+# for i in range(a):
+#     for j in range(i,a):
+#         print(j,end=' ')
+#     print()
+
+# a=int(input('enter rows:'))
+# for i in range(0,a,-1):
+#     for j in range(i,a):
+#         print(j,end=' ')
+#     print()
